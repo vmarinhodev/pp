5d69582e052116555d26d84d726800e494ed6202
Vitor Marinho <vitor@uw-s.nl>
2024-05-02 10:25:31 +0200

    updated ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,6 +6,7 @@ export const wrapPageElement = ({ element, props }) => {
   return (
     <Location>
       {({ location }) => <Wrapper {...props}>{element}</Wrapper>}
+      
     </Location>
   )
 }
